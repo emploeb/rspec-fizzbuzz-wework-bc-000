6966cbd1f55dff7c991b62b1e0280d0b8d11ed66
Emma Loeb <emma.loeb@wework.com>
2018-02-18 20:53:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz (fizz_number)
   if ((fizz_number % 3 == 0) && (fizz_number % 5 == 0))
-    "FizzBuzz
+    "FizzBuzz"
   elsif fizz_number % 3 == 0
     "Fizz"
   elsif fizz_number % 5 == 0 
